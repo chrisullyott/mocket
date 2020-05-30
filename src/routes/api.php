ae9a79f91c2940b4a0314b33c69ac026feed6c47
Chris Ullyott <contact@chrisullyott.com>
2020-05-30 06:42:11 -0700

    Add basic API endpoints

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Use App\Item;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Create an item.
+// Route::put('item', function(Request $request) {
+//     $model = Item::create([
+//         'user_id' => 'NEED AUTH',
+//         'url' => $request->input('url')
+//     ]);
+
+//     return $model;
+// });
+
+// Get an item by ID.
+Route::get('items/{id}', function($id) {
+    return Item::findOrFail($id);
+});
+
+// Get all items.
+Route::get('items', function() {
+    return Item::all();
+});
+
+// Update an item.
+Route::put('items/{id}', function(Request $request, $id) {
+    $model = Item::findOrFail($id);
+    $model->update($request->all());
+    return $model;
+});
+
+// Delete an item.
+Route::delete('items/{id}', function(Request $request, $id) {
+    $model = Item::findOrFail($id);
+    $model->meta()->delete();
+    $model->delete();
+    return response()->json(null, 204);
 });
