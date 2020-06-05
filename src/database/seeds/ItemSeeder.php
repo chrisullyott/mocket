4d2bc446e4fde04339716c4557e4b6f3b521d59d
Chris Ullyott <contact@chrisullyott.com>
2020-06-05 10:59:38 -0700

    Remove some default items

--- src/database/seeds/ItemSeeder.php
+++ src/database/seeds/ItemSeeder.php
@@ -14,16 +14,12 @@ class ItemSeeder extends Seeder
     {
         $urls = [
             'https://blog.mint.com/how-to/identity-theft-protection/',
-            'http://troels.arvin.dk/db/rdbms/',
             'https://goinswriter.com/remarkable/',
             'https://www.youtube.com/watch?v=tVooja0Ta5I',
             'https://michaelhyatt.com/how-to-beat-a-burnout-culture/',
-            'https://www.ted.com/talks/sascha_morrell_why_should_you_read_moby_dick',
             'https://laracasts.com/podcast/318183',
             'https://www.youtube.com/watch?v=WPoQfKQlOjg',
-            'https://laracasts.com/series/testing-vue/episodes/2',
             'https://www.codeforamerica.org/work',
-            'https://vuejs.org/v2/guide/',
             'https://www.youtube.com/watch?v=IS62jMJDzgg#t=42m18s',
             'https://www.ted.com/talks/brene_brown_the_power_of_vulnerability',
             'http://chrisullyott.com/help-the-amazon-rainforest'
