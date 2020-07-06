0cb99479a93cf57d9bda2ad69f08490a92d7c48b
Chris Ullyott <contact@chrisullyott.com>
2020-07-06 07:20:32 -0700

    Add basic ItemTest

---
+++ src/tests/Feature/ItemTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Item;
+use UserSeeder;
+
+class ItemTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Run test setup.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(UserSeeder::class);
+    }
+
+    /**
+     * An item can be added to the database.
+     *
+     * @return void
+     */
+    public function testAnItemCanBeAdded(): void
+    {
+        $url = 'https://laravel.com/docs/7.x';
+        $item = Item::create(['user_id' => 1, 'url' => $url]);
+
+        $this->assertTrue($item->exists);
+    }
+}
