d32afa9dc0701a32cb2cee21899fbb5c0aecd791
Chris Ullyott <contact@chrisullyott.com>
2020-07-08 17:02:48 -0700

    Make LoginTest

---
+++ src/tests/Feature/LoginTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Run test setup.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(UserSeeder::class);
+    }
+
+    /**
+     * @test A user is redirected to the login screen.
+     * @return void
+     */
+    public function user_is_redirected_to_login(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertRedirect('/login');
+    }
+
+    /**
+     * @test An existing user can log in and is taken to the home screen.
+     * @return void
+     */
+    public function existing_user_can_log_in(): void
+    {
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/');
+    }
+}
